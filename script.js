1e41e72a63253e1978db2020f3d46a49627c7394
Denny Pearce <code.coffee.creator@gmail.com>
2023-05-15 14:30:37 +0300

    add main screen

--- script.js
+++ script.js
@@ -24,3 +24,37 @@ const addClass = document.querySelector('.site');
 sTrigger.addEventListener('click', () => {
   addClass.classList.toggle('showsearch');
 });
+
+const sliderThumb = new Swiper('.thumb-nav', {
+  spaceBetween: 10,
+  slidesPerView: 3,
+  slidesPerGroup: false,
+  breakpoints: {
+    992: {
+      direction: 'vertical',
+    },
+  },
+});
+
+const theSlider = new Swiper('.thumb-big', {
+  slidePerView: 1,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  thumbs: {
+    swiper: sliderThumb,
+  },
+});
+
+const io = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.intersectionRatio > 0) {
+      entry.target.classList.add('this');
+    }
+  });
+});
+
+const box = document.querySelectorAll('.animate');
+box.forEach((el) => {
+  io.observe(el);
+});
